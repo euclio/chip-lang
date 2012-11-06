3b9bd299ac07cf906da7a6e57f1c2336326a709f
Andy Russell <arussell123@gmail.com>
2012-11-05 19:32:29 -0800

    Updated intermediate representation to current state of syntax

--- ChiptuneDSL/src/chipLang/IR.scala
+++ ChiptuneDSL/src/chipLang/IR.scala
@@ -3,36 +3,41 @@ package chipLang.ir
 object constants {
   val DefaultTimeSig = TimeSignature(4, 4)
   val DefaultBPM = BPM(160)
+  val DefaultOptions = Options(DefaultTimeSig, DefaultBPM)
 }
 
-case class Song(ps: List[Verse])
+sealed abstract class Identifier
+case class PhraseIdentifier extends Identifier
+case class VerseIdentifier extends Identifier
 
-case class Verse(ts: TimeSignature = constants.DefaultTimeSig, bpm: BPM = constants.DefaultBPM, i: Instrument = Square1, ps: List[Phrase])
+case class Song(ps: List[Phrase])
 
-abstract class Instrument
-object Square1 extends Instrument
-object Square2 extends Instrument
-object Square3 extends Instrument
-object Square4 extends Instrument
-object Triangle extends Instrument
-object Saw extends Instrument
-object Sine extends Instrument
-object WhiteNoise extends Instrument
-object SquareDown extends Instrument
+case class Phrase(opts: Options = constants.DefaultOptions, ps: List[Verse])
 
-case class Phrase(ns: List[Notation])
-
+case class Options(ts: TimeSignature, bpm: BPM)
+case class BPM(bpm: Int) { require(40 <= bpm && bpm <= 200 && bpm % 10 == 0) }
 case class TimeSignature(top: Int, bot: Int)
-case class BPM(bpm: Int)
 
-abstract class Notation
+case class Verse(inst: Instrument, channels: List[List[Notation]])
+
+sealed abstract class Instrument
+case object Square1 extends Instrument
+case object Square2 extends Instrument
+case object Square3 extends Instrument
+case object Square4 extends Instrument
+case object Triangle extends Instrument
+case object Saw extends Instrument
+case object Sine extends Instrument
+case object WhiteNoise extends Instrument
+case object SquareDown extends Instrument
+
+sealed abstract class Notation
 case class Rest(d: Duration) extends Notation
-case class Sound(o: Octave, p: Pitch, i: Accidental = Natural, d: Duration = Duration(1)) extends Notation
+case class Sound(o: Octave, p: Pitch, a: Accidental = Natural, d: Duration = Duration(1)) extends Notation
 
 case class Pitch(p: Char) { require('A' <= p && p <= 'G') }
 case class Octave(o: Int) { require(0 <= o && o < 9) }
-case class Duration(d: Int)
-
+case class Duration(d: Double)
 sealed abstract class Accidental
 case object Sharp extends Accidental
 case object Flat extends Accidental
