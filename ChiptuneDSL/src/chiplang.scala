d2b711e0e75093a6f7fe33cbaa2214057f341d8d
Andy Russell <arussell123@gmail.com>
2012-12-02 23:56:43 -0800

    Added main program entry point

--- ChiptuneDSL/src/chiplang.scala
+++ ChiptuneDSL/src/chiplang.scala
@@ -3,16 +3,21 @@ import scala.io.Source
 import chipLang.semantics._
 
 object ChipCompiler extends App {
-  require(args.length == 1, "Too many arguments.")
-  
+  require(args.length == 1, "Usage: chiplang [ .cl file ].")
+
   val program = Source.fromFile(args(0)).mkString
-  
+
   ChipParser(program) match {
     case ChipParser.Success(ir, _) => {
-      println("Parsed successfully. Compiling...")
-      eval(ir)
+      println(args(0) + " parsed successfully.")
+      println("Compiling...")
+      try {
+        compile(ir)
+      }
+
+      println("Compiled output.midi successfully.")
     }
-    
+
     case ChipParser.NoSuccess(msg, _) => {
       println("Parsing failed. " + msg)
     }
